d24be728f04792fce05c27ee3b6b327d0ea83f29
Leader <leaderkhiva@gmail.com>
2025-09-16 21:37:52 +0500

    asosiy constantalar alohida faylga yozildi

--- constants.php
+++ constants.php
@@ -1,5 +1,9 @@
 <?php
 
 // elementlarni faol / nofaol qilish belgilari (flag)
-const STATUS_ACTIVE = 1;
-const STATUS_INACTIVE = 0;
+const STATUS_ACTIVE = 1;    // faol element flag (bayrog'i)
+const STATUS_INACTIVE = 0;  // nofaol element flagi
+
+const MENU_MODEL = __DIR__ . '/models/menusModel.php';      // menu lar modeli joylashgan modeli yo'li
+const SOCIAL_MODEL = __DIR__ . '/models/socialsModel.php';  // ijtimoiy tarmoqlar modeli joylashgan yo'li
+const POST_MODEL = __DIR__ . '/models//bannersModel.php';     // banner modeli joylashgan yo'li
